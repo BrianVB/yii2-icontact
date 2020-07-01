c292f2ceb320b2481a551a32d0d5efb528026a6c
BVBAccelm <brian@accelm.com>
2020-07-01 17:27:35 -0400

    add backend controller w/action to sync contact via api

--- src/backend/controllers/ContactController.php
+++ src/backend/controllers/ContactController.php
@@ -4,7 +4,9 @@ namespace bvb\icontact\backend\controllers;
 
 use bvb\icontact\common\models\Contact;
 use bvb\user\backend\controllers\traits\AdminAccess;
+use Yii;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 /**
  * @todo This has yet to be completed and also poses some challenges because the
@@ -20,14 +22,25 @@ class ContactController extends Controller
     use AdminAccess;
 
     /**
-     * Syncs contact data
-     * @param string $id ID of the contact in iContact
-     * @param string $redirectUrl URL to redirect a user to after completion
+     * Syncs contact data by just using validate() on the model to trigger
+     * beforeValidate() where it all happens
+     * @param string $userId ID of the contact in iContact
+     * @param string $redirectUrl URL to redirect the uesr to
      * @return yii\web\Response
      */
-    public function actionSync($id, $redirectUrl)
+    public function actionSync($userId, $redirectUrl)
     {
-        $this->makeCall("/a/{$this->setAccountId()}/c/{$this->setClientFolderId()}/contacts/{$iContactId}", 'POST', $aContact, 'contact');
+        $contact = Contact::findOne($userId);
+        if(!$contact){
+            throw new NotFoundHttpException('Contact not found');
+        }
+        if($contact->validate()){
+            Yii::$app->session->addFlash('success', 'Contact updated in iContact');
+        } else {
+            $message = 'There was an error updating the contact in iContact';
+            Yii::error($message.': '.print_r($contact->getErrors(), true));
+            Yii::$app->session->addFlash('error', $message);
+        }
         return $this->redirect($redirectUrl);
     }
 }
